3444e0cffc7e49691289c39217d0c813108e30b2
Qoter <tsalapoval@gmail.com>
2019-09-04 17:50:10 +0300

    m

--- stayfrosybeats/anim.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-  var headerHeight = document. getElementById('header').offsetHeight;
-    $('a[href^="#"]').on('click', function(event) {
-      event.preventDefault();
-      var sc = $(this).attr("href"),
-          dn = $(sc).offset().top - headerHeight;
-      $('html, body').animate({scrollTop: dn}, 1000);
-    });
-  });
